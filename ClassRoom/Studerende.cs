c065786cf07106560d00e9fb0bc60950009db6a6
Patrick Thomas Ørum <Patr421b@edu.easj.dk>
2018-09-05 15:05:30 +0200

    added properties, and constructor to class Studerende

---
+++ ClassRoom/Studerende.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class Studerende
+    {
+        public String Navn { get; private set; }
+        public int Fødselsmåned { get; private set; }
+        public int Fødselsdag { get; private set; }
+
+        public Studerende(string navn, int fødselsmåned, int fødselsdag)
+        {
+            Navn = navn;
+            Fødselsmåned = fødselsmåned;
+            Fødselsdag = fødselsdag;
+        }
+    }
+}
